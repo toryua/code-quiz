ed6786ba9b8109dc4b897375445ce67fc91c1735
toryua <87789457+toryua@users.noreply.github.com>
2021-08-28 16:01:58 -0700

    created and connected files

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// test button clickto console
+var startClick = alert("The timer has started and the game has begun!")
+
+document.getElementById("#start-btn").addEventListener("click", startClick) 
+
+    console.log("the quiz has begun!")
